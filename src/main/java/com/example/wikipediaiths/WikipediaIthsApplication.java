175f0facae0b8e7b5e57eb8621aa820b60eb448b
Depinder Kaur <depinder.kaur@iths.se>
2024-01-13 20:21:33 +0100

    Added the final (group) project of the Database course, ITHS-JU2023.

---
+++ src/main/java/com/example/wikipediaiths/WikipediaIthsApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikipediaiths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaIthsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikipediaIthsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wikipediaiths/WikipediaIthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikipediaiths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaIthsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
